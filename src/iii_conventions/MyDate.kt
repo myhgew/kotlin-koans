7ce91865191e8655d45103005ca5c5890ca470e0
Yuhua Mai <yuhumai@cisco.com>
2017-08-16 13:36:23 -0700

    Finish n31

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,37 @@
 package iii_conventions
 
+
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.compareTo(other: MyDate): Int {
+//    if (this.year == other.year) {
+//        if (this.month == other.month) {
+//            return this.dayOfMonth - other.dayOfMonth
+//        } else {
+//            return this.month - other.month
+//        }
+//    } else {
+//        return this.year - other.year
+//    }
+
+    return when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval) : MyDate = this.addTimeIntervals(timeInterval, 1)
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) : MyDate = this.addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.number)
+
+
+
+
+
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +39,27 @@ enum class TimeInterval {
     YEAR
 }
 
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+operator fun TimeInterval.times(number: Int) : RepeatedTimeInterval = RepeatedTimeInterval(this, number)
+
+
+
+
+
 class DateRange(val start: MyDate, val endInclusive: MyDate)
+
+operator fun DateRange.contains(d: MyDate): Boolean {
+    return d >= start && d <= endInclusive
+}
+
+operator fun DateRange.iterator(): Iterator<MyDate> = DateIterator(this)
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+}
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,20 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable( var invocations: Int = 0)
+
+operator fun Invokable.invoke(): Invokable = Invokable(this.invocations + 1)
+fun Invokable.getNumberOfInvocations(): Int = this.invocations
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
+
