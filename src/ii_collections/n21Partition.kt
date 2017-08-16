0fbe7bc3655ed15b014d9daf5a662bb08de96aee
Yuhua Mai <yuhumai@cisco.com>
2017-08-16 11:47:46 -0700

    Finish n24

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return this.customers.filter {
+        val (deliveredOrders, notDeliveredOrders) = it.orders.partition { it.isDelivered }
+        notDeliveredOrders.size > deliveredOrders.size
+    }.toSet()
 }
